279be33688d5961995976e6a434e5e647b464a42
Élaff Ramos <valelaff10@gmail.com>
2024-07-12 08:25:06 -0300

    Create calculo-área-círculo.py

---
+++ calculo-área-círculo.py
@@ -0,0 +1,9 @@
+# Calculo da área 
+
+print('***CALCULANDO A ÁREA DO RETÂNGULO***')
+
+base = int(input('\nDigite o valor da aréa: '))
+
+largura = float(input('Digite o valor da largura: '))
+
+print("\nAréa do retangulo "+str(base*largura))
